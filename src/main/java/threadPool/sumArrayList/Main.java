5c394b2260fadcf4847962c24e5eb4ae13ddf1d1
MaksMusic <Maksmusic05@yandex.ru>
2023-09-08 16:36:02 +0300

    work with ThreadPool task search sum elements to List

---
+++ src/main/java/threadPool/sumArrayList/Main.java
@@ -0,0 +1,21 @@
+package threadPool.sumArrayList;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = DataBase.arrayList.parallelStream().mapToInt(e-> e).sum();
+        System.out.println(sum);
+    }
+
+}
+
+
+
+
+class DataBase{
+    static List<Integer> arrayList =
+            Arrays.asList(1,49,32,21,12,43,23,43,234,23,5,7,867,4,3,23,65,345,756,45,100);
+
+}
---
+++ src/main/java/threadPool/test/Main.java
@@ -0,0 +1,39 @@
+package threadPool.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        executorService.execute(new MyRun("загрузка 1"));
+        executorService.execute(new MyRun("загрузка 2"));
+        executorService.execute(new MyRun("загрузка 3"));
+        executorService.execute(new MyRun("загрузка 4"));
+        executorService.execute(new MyRun("загрузка 5"));
+
+    }
+}
+
+class MyRun implements Runnable{
+    String text;
+
+    public MyRun(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(text);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
